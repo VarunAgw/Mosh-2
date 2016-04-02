5ca222d03e7f3dffae8b668be323717ecb6800de
Varun Agrawal <Varun@VarunAgw.com>
2016-04-02 22:59:35 +0530

    Updated installer to install mosh-reset too

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ echo "Mosh 2 installation started"
 
 wget -q "https://raw.githubusercontent.com/VarunAgw/Mosh-2/master/mosh" -O /usr/local/bin/mosh
 wget -q "https://raw.githubusercontent.com/VarunAgw/Mosh-2/master/mosh-cache" -O /usr/local/bin/mosh-cache
+wget -q "https://raw.githubusercontent.com/VarunAgw/Mosh-2/master/mosh-reset" -O /usr/local/bin/mosh-reset
 
 chmod a+x /usr/local/bin/mosh
 chmod a+x /usr/local/bin/mosh-cache
+chmod a+x /usr/local/bin/mosh-reset
 
 echo "Mosh 2 installed succesfully. Please restart your shell program!"
